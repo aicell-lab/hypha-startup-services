17600817508c080fcedf38c3d83807d693f9abaf
Hugo Dettner Källander <hugodk@kth.se>
2025-06-05 12:06:19 +0200

    Add progress get_mem0

--- hypha_startup_services/mem0_service/mem0_client.py
+++ hypha_startup_services/mem0_service/mem0_client.py
@@ -1,15 +1,6 @@
 from mem0 import AsyncMemory
 from dotenv import load_dotenv
 
-# client = WeaviateAsyncClient(
-#     additional_config=AdditionalConfig(
-#         timeout_config=(5, 60),  # (connection_timeout_sec, request_timeout_sec)
-#         grpc_insecure=True,  # Skip SSL verification for gRPC
-#         grpc_insecure_auth=True,  # Allow insecure authentication
-#     ),
-#     skip_init_checks=True,
-# )
-
 
 async def get_mem0() -> AsyncMemory:
     """
@@ -18,16 +9,12 @@ async def get_mem0() -> AsyncMemory:
     """
     load_dotenv()
 
-    grpc_url = "hypha-weaviate-grpc.scilifelab-2-dev.sys.kth.se"
-    http_url = "hypha-weaviate.scilifelab-2-dev.sys.kth.se"
-
     config = {
         "vector_store": {
             "provider": "weaviate",
             "config": {
                 "collection_name": "Document",
-                "cluster_url": http_url,
-                # "auth_client_secret": os.environ["WEAVIATE_ADMIN_KEY"],
+                "cluster_url": "localhost:8080",
             },
         }
     }
